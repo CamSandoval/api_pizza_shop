aef16b8f945ca6a210dd0aa39b65384108bdc63e
CamSandoval <juanprogram06@gmail.com>
2023-10-09 17:15:04 -0500

    feat(/*):This is the first configuration of the proyect

---
+++ src/main/java/com/platzi/pizza/PlatziPizzaShopApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziPizzaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziPizzaShopApplication.class, args);
+	}
+
+}
